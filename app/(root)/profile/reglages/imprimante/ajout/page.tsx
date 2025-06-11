05be7c422a2576996b9400bd9d0b38e8949bc682
Filip Petrovic <filippetrovicfr04@gmail.com>
2025-06-11 11:39:53 +0200

    TypeScript Erreur Fix

--- app/(root)/profile/reglages/imprimante/ajout/page.tsx
+++ app/(root)/profile/reglages/imprimante/ajout/page.tsx
@@ -1,12 +1,7 @@
-import PrinterFormAdd from "@/components/Printer/PrinterFormAdd";
 import React from "react";
 
 const ProfileImprimanteAjout = () => {
-  return (
-    <div>
-      <PrinterFormAdd />
-    </div>
-  );
+  return <div>Test</div>;
 };
 
 export default ProfileImprimanteAjout;
