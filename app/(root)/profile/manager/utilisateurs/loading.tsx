cb13a3c35ae9af91081dec2239b7f1210a38cf76
Filip Petrovic <filippetrovicfr04@gmail.com>
2025-10-29 15:38:28 +0100

    Ajout des pages loading sur la module /profile

---
+++ app/(root)/profile/manager/utilisateurs/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen py-10">
+      <div className="animate-spin rounded-full h-24 w-24 border-t-2 border-b-2 border-blue-500" />
+    </div>
+  );
+};
+
+export default Loading;
